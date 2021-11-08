c5a60e6c1c3a35c1c5f1cd86fdf55873e807a0e8
Abha97 <93879146+Abha97@users.noreply.github.com>
2021-11-08 23:47:14 +0530

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,7 +1,29 @@
+
 import cv2
 
  
 
 img = cv2.imread("/Task01.jpg")
-
 cv2_imshow(img)
+
+Original_Image = cv2.cvtColor(img, 0)
+New_Image = cv2.GaussianBlur(Original_Image, (3,3), 0)
+
+ 
+
+sobelx = cv2.Sobel(src=New_Image, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
+sobely = cv2.Sobel(src=New_Image, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
+sobelxy = cv2.Sobel(src=New_Image, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+
+ 
+
+cv2_imshow(sobelx)
+cv2_imshow(sobely)
+cv2_imshow(sobelxy)
+
+ 
+
+edges = cv2.Canny(image=New_Image, threshold1=120, threshold2=240)
+cv2_imshow(edges)
+
+cv2.destroyAllWindows()
